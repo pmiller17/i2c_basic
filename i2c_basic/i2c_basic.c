e12af612e56c716b484d1d916e62d7abdc9055c0
Paul Miller <pmiller17@gmail.com>
2014-08-07 18:56:34 -0400

    First publish
    AVR310 code included, with main example C file renamed and not actually
    part of the code. Still to be included is a separate Arduino sketch
    file. USI_TWI_Master.h had some #defines in in place of the conditional
    defines for USI port definitions. some IAR language was changed to
    avr-gcc, such as reference to __x, and libraries inavr.h, ioavr.h.
    Changed to <avr/io.h>

---
+++ i2c_basic/i2c_basic.c
@@ -0,0 +1,15 @@
+/*
+ * i2c_basic.c
+ *
+ * Created: 7/14/2014 5:04:45 PM
+ *  Author: mini me
+ */ 
+
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
